b58712569d8348ebd68a05500cbc77a2ca7b68f8
K-Minhyeok <gurdl2384@naver.com>
2024-01-24 20:20:13 +0900

    A50 추가

--- quiz/A050_김민혁_20240124.java
+++ quiz/A050_김민혁_20240124.java
@@ -6,13 +6,18 @@ public class A050_김민혁_20240124 {
 
     public static void main(String[] args) {
 
+
         Scanner sc = new Scanner(System.in);
         String oneLine = sc.nextLine();
         char[] beforeConv = oneLine.toCharArray();
         char[] afterConv = new char[beforeConv.length];
         for(int i =0; i<oneLine.length();i++){
             byte invert = (byte)beforeConv[i];
-            invert += 3;
+            if(invert<68){
+                invert+=23;
+            }else {
+                invert -= 3;
+            }
             afterConv[i] = (char)invert;
             System.out.print(afterConv[i]);
         }
---
+++ quiz/A051_김민혁_20240124.java
@@ -0,0 +1,6 @@
+package quiz;
+
+public class A051_김민혁_20240124 {
+
+
+}
