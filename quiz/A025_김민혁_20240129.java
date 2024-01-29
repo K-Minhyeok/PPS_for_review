f344b6ccc4a623f25399d9d235856e7b087ea209
K-Minhyeok <gurdl2384@naver.com>
2024-01-29 19:20:31 +0900

    A25추가 : 40번째

---
+++ quiz/A025_김민혁_20240129.java
@@ -0,0 +1,15 @@
+package quiz;
+
+public class A025_김민혁_20240129 {
+
+    class Solution {
+        public boolean isPowerOfFour(int n) {
+
+            if(n%4==0 || n==1){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+}
