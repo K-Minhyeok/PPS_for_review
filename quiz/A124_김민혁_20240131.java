14226c5ee08cd113f68a5f25de9a9770d32a98f1
K-Minhyeok <gurdl2384@naver.com>
2024-01-31 11:15:44 +0900

    A123추가 : 46번

--- quiz/A124_김민혁_20240131.java
+++ quiz/A124_김민혁_20240131.java
@@ -6,24 +6,24 @@ import java.util.List;
 
 import java.util.Scanner;
 
-public class Main {
+public class A124_김민혁_20240131 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         int div = 2;
         List<Integer> result = new ArrayList<>();
-        while(num!=1){
-            if(num%div ==0){
+        while (num != 1) {
+            if (num % div == 0) {
                 result.add(div);
-                num/=div;
-            }else{
+                num /= div;
+            } else {
                 div++;
             }
         }
         Collections.sort(result);
 
-        for(int i=0; i< result.size();i++){
+        for (int i = 0; i < result.size(); i++) {
             System.out.println(result.get(i));
         }
     }
