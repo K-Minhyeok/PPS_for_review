5a38fafff5395c907fe30f97b299d82a300199b5
K-Minhyeok <gurdl2384@naver.com>
2024-01-31 10:33:49 +0900

    A110추가 : 44번

--- quiz/A109_김민혁_20240131.java
+++ quiz/A109_김민혁_20240131.java
@@ -1,11 +1,8 @@
 package quiz;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-import java.math.BigInteger;
 
-public class Main {
+import java.util.Scanner;
+
+public class A109_김민혁_20240131 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
