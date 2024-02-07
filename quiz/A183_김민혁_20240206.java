1dac6623412e92be559198ea229fc1085bf80ee8
K-Minhyeok <gurdl2384@naver.com>
2024-02-07 20:32:19 +0900

    A183추가 : 62번째

---
+++ quiz/A183_김민혁_20240206.java
@@ -0,0 +1,12 @@
+package quiz;
+
+public class A183_김민혁_20240206 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
