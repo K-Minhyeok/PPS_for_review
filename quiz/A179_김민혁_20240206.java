13e431d4ecc5dbc42bbf3baea070a08e1a0be262
K-Minhyeok <gurdl2384@naver.com>
2024-02-07 20:25:23 +0900

    A179추가 : 59번째

---
+++ quiz/A179_김민혁_20240206.java
@@ -0,0 +1,15 @@
+package quiz;
+import java.util.Scanner;
+
+public class A179_김민혁_20240206 {
+
+    public class Main{
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int year = sc.nextInt();
+
+            int age = year-543;
+            System.out.println(age);
+        }
+    }
+}
