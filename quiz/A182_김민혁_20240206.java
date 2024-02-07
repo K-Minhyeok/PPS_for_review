1b2170719288d9c899ac2f4835b8f33a6e002716
K-Minhyeok <gurdl2384@naver.com>
2024-02-07 20:31:14 +0900

    A182추가 : 61번째

---
+++ quiz/A182_김민혁_20240206.java
@@ -0,0 +1,17 @@
+package quiz;
+
+import java.util.Scanner;
+
+public class A182_김민혁_20240206 {
+
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            long firstIn = sc.nextLong();
+            long sencondIn = sc.nextLong();
+            long lastIn = sc.nextLong();
+            System.out.println(firstIn + sencondIn + lastIn);
+        }
+    }
+}
