6b594275f1747920e58304f3375d7a65d18e9800
K-Minhyeok <gurdl2384@naver.com>
2024-02-07 20:36:19 +0900

    A186추가 : 65번째

---
+++ quiz/A186_김민혁_20240207.java
@@ -0,0 +1,26 @@
+package quiz;
+
+import java.util.Scanner;
+
+public class A186_김민혁_20240207 {
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int score = sc.nextInt();
+
+            if (score <= 100 && score >= 90) {
+                System.out.println("A");
+            } else if (score <= 89 && score >= 80) {
+                System.out.println("B");
+            } else if (score <= 79 && score >= 70) {
+                System.out.println("C");
+            } else if (score <= 69 && score >= 60) {
+                System.out.println("D");
+            } else {
+                System.out.println("F");
+            }
+            sc.close();
+        }
+    }
+}
